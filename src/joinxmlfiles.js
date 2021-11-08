eebc927147de39a5424fdf86aaa60c3c0cdaf3ee
Nullish <richerde43@gmail.com>
2021-11-08 23:35:24 +0000

    Make it just a function. No CLI access.

--- src/joinxmlfiles.js
+++ src/joinxmlfiles.js
@@ -1,3 +1,10 @@
+const fs = require('fs');
+    const path = require('path');
+    const {
+        DOMParser,
+        DOMImplementation
+    } = require('@xmldom/xmldom');
+    
 /* ***** TODO
 
 - Detect child type to dynamically select xml type nodes vs process instructions
@@ -7,35 +14,15 @@
 ******
 */
 
-const fs = require('fs');
-const path = require('path');
-const {
-    DOMParser,
-    DOMImplementation
-} = require('@xmldom/xmldom');
-
 /* **
 	* @param {string} dir - directory of XML files
-	* @param {boolean} writeout - TRUE if output should be written to a file.
 
 	xmldom documentation: https://github.com/xmldom/xmldom
 
 */
-const joinxmlfiles = (...args) => {
-    // Joins multiple XML files
-    const dir = args[0] || process.argv[2]
-    const rawData = getDataFromFiles(dir);
-    // Act on argument to suppress file output
-    /*
-    if (args[1] !== "no-file") {
-    	writeFile(dir, rawData);
-    }
-    */
-    console.log(rawData.toString());
-    return rawData;
-}
 
-const getDataFromFiles = (dir) => {
+const joinxmlfiles = (...args) => {
+    const dir = args[0] || process.argv[2]
     let xmlJoined = new DOMImplementation().createDocument();
     const fileNames = fs.readdirSync(dir);
     fileNames.forEach(fileName => {
@@ -49,21 +36,13 @@ const getDataFromFiles = (dir) => {
             const parser = new DOMParser();
             const xmlFile = parser.parseFromString(fileString, 'text/xml');
             for (i = 0; i < xmlFile.childNodes.length; i++) {
-            	if (xmlFile.childNodes[i].nodeType == 1) {
-                xmlJoined.appendChild(xmlFile.childNodes[i]);
-            	}
+                if (xmlFile.childNodes[i].nodeType == 1) {
+                    xmlJoined.appendChild(xmlFile.childNodes[i]);
+                }
             }
         }
     });
     return xmlJoined;
 }
 
-function writeFile(dir, manifest) {
-    // Receives a string and writes it to file in local file system
-    const fileName = 'manifest.json'
-    const filePath = path.join(dir, fileName)
-    fs.writeFileSync(filePath, manifest)
-}
-
-joinxmlfiles();
 module.exports = joinxmlfiles
