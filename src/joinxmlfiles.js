9074bd587daa3961698500ad5ff31cc716226cec
Nullish <richerde43@gmail.com>
2021-11-08 23:14:48 +0000

    Dynamically append only element type nodes

--- src/joinxmlfiles.js
+++ src/joinxmlfiles.js
@@ -1,6 +1,18 @@
+/* ***** TODO
+
+- Detect child type to dynamically select xml type nodes vs process instructions
+	Do by checking if nodeType = 1 (Element)
+- Elegant write out to file
+
+******
+*/
+
 const fs = require('fs');
 const path = require('path');
-const { DOMParser, DOMImplementation } = require('@xmldom/xmldom');
+const {
+    DOMParser,
+    DOMImplementation
+} = require('@xmldom/xmldom');
 
 /* **
 	* @param {string} dir - directory of XML files
@@ -10,44 +22,47 @@ const { DOMParser, DOMImplementation } = require('@xmldom/xmldom');
 
 */
 const joinxmlfiles = (...args) => {
-	// Joins multiple XML files
-	const dir = args[0] || process.argv[2]
-	const rawData = getDataFromFiles(dir);
-	// Act on argument to suppress file output
-	/*
-	if (args[1] !== "no-file") {
-		writeFile(dir, rawData);
-	}
-	*/
-	console.log(rawData);
-	return rawData;
+    // Joins multiple XML files
+    const dir = args[0] || process.argv[2]
+    const rawData = getDataFromFiles(dir);
+    // Act on argument to suppress file output
+    /*
+    if (args[1] !== "no-file") {
+    	writeFile(dir, rawData);
+    }
+    */
+    console.log(rawData.toString());
+    return rawData;
 }
 
 const getDataFromFiles = (dir) => {
-	let xmlJoined = new DOMImplementation().createDocument();
-	const fileNames = fs.readdirSync(dir);
-	fileNames.forEach(fileName => {
-		
-		// Process XML file only if it's not zero length
-		const stats = fs.statSync(`${dir}${fileName}`);
-		const fSize = stats["size"];
-		if (fSize > 0 && fileName.match(/\.xml$/)) {
-			const fileString = fs.readFileSync(`${dir}${fileName}`, 'utf-8');
-			// console.log(fileString);
-			const parser = new DOMParser();
-			const xmlFile = parser.parseFromString(fileString, 'text/xml');
-			const xmlBody = xmlFile.lastChild;
-			xmlJoined.appendChild(xmlBody);
-		}
-	});
-	return xmlJoined.toString();
+    let xmlJoined = new DOMImplementation().createDocument();
+    const fileNames = fs.readdirSync(dir);
+    fileNames.forEach(fileName => {
+
+        // Process XML file only if it's not zero length
+        const stats = fs.statSync(`${dir}${fileName}`);
+        const fSize = stats["size"];
+        if (fSize > 0 && fileName.match(/\.xml$/)) {
+            const fileString = fs.readFileSync(`${dir}${fileName}`, 'utf-8');
+            // console.log(fileString);
+            const parser = new DOMParser();
+            const xmlFile = parser.parseFromString(fileString, 'text/xml');
+            for (i = 0; i < xmlFile.childNodes.length; i++) {
+            	if (xmlFile.childNodes[i].nodeType == 1) {
+                xmlJoined.appendChild(xmlFile.childNodes[i]);
+            	}
+            }
+        }
+    });
+    return xmlJoined;
 }
 
-function writeFile (dir, manifest) {
-	// Receives a string and writes it to file in local file system
-	const fileName = 'manifest.json'
-	const filePath = path.join(dir, fileName)
-	fs.writeFileSync(filePath, manifest)
+function writeFile(dir, manifest) {
+    // Receives a string and writes it to file in local file system
+    const fileName = 'manifest.json'
+    const filePath = path.join(dir, fileName)
+    fs.writeFileSync(filePath, manifest)
 }
 
 joinxmlfiles();
